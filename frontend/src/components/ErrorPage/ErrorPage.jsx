494d5cbf39c43272686da3423e7e2eca084de813
remnantsofred <remnantsofred@gmail.com>
2022-12-31 15:21:39 -0800

    Begin error page styling

--- frontend/src/components/ErrorPage/ErrorPage.jsx
+++ frontend/src/components/ErrorPage/ErrorPage.jsx
@@ -7,16 +7,15 @@ import Panels from '../panels'
 export const ErrorPage = ({children, id='', className="errorPage"}) => {
   return (
     <div className={className} id={id}>
-      <h1>404</h1>
-        {children}
-      <p>
-        Whoops!
-        <br />
-        That's not how we wanted this to go.
-      </p>
       <div className="imagePanel">
         <img src={kettlebell} alt="" id="kettlebell"/>
         <img src={arm} alt="" id="arm"/>
+        <h1 className='errorMsg'>404</h1>
+        <p>
+          Whoops!
+          <br />
+          That's not how we wanted this to go.
+        </p>
       </div>
     </div>  
   );
