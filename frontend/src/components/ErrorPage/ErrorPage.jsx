14a87d3af0112ce5f0dcb158673ae8d670c37a51
remnantsofred <remnantsofred@gmail.com>
2023-01-02 12:13:56 -0800

    Error page styling

--- frontend/src/components/ErrorPage/ErrorPage.jsx
+++ frontend/src/components/ErrorPage/ErrorPage.jsx
@@ -3,21 +3,27 @@ import arm from './error_page_arm.png'
 import kettlebell from './error_page_kettlebell.png'
 import Panel from '../panel/Panel'
 import Panels from '../panels'
+import Column from '../column/Column'
 
 export const ErrorPage = ({children, id='', className="errorPage"}) => {
   return (
+
     <div className={className} id={id}>
-      <div className="imagePanel">
-        <img src={kettlebell} alt="" id="kettlebell"/>
-        <img src={arm} alt="" id="arm"/>
-        <h1 className='errorMsg'>404</h1>
-        <p>
-          Whoops!
-          <br />
-          That's not how we wanted this to go.
-        </p>
-      </div>
-    </div>  
+        
+          <img src={arm} alt="" id="arm"/>
+ 
+          <div className='errorSection'>
+            <h1 className='errorMsg'>404</h1>
+            <p className='whoops'>
+              Whoops!
+              <br />
+              That's not how we wanted this to go.
+            </p>
+          </div>
+          <img src={kettlebell} alt="" id="kettlebell"/>
+      
+    </div>
+     
   );
 } 
 
