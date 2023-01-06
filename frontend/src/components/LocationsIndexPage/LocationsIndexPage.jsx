19218446c8da1595550e06493994bd59969b94cb
remnantsofred <remnantsofred@gmail.com>
2023-01-06 11:35:02 -0800

    Clean up width sizing left panels Lessons, Locations

--- frontend/src/components/LocationsIndexPage/LocationsIndexPage.jsx
+++ frontend/src/components/LocationsIndexPage/LocationsIndexPage.jsx
@@ -8,33 +8,58 @@ import Panel from '../panel/Panel';
 import Columns from '../columns/Columns';
 import Column from '../column/Column';
 import Row from '../row/Row';
+import { getLessons, fetchLessons } from '../../store/lesson';
 import { getLocations, fetchLocations } from '../../store/location';
 import LocationIndexItem from '../LocationIndexItem/LocationIndexItem';
+import Loading from '../loading/Loading';
+import Map from '../map';
 
 export const LocationsIndexPage = ({children, id='', className="LocationsIndexPage"}) => {
+  const lessons = useSelector(getLessons);
   const locations = useSelector(getLocations);
   const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
+    dispatch(fetchLessons())
     dispatch(fetchLocations())
-  }, [dispatch])
+  }, [])
+
+  useEffect(() => {
+    if (locations && lessons) {
+      setLoaded(true)
+    }
+  },[dispatch, locations, lessons])
+
+  const getLocation = (locationId) => {
+    for (const location of locations) {
+      if (location.id === locationId) {
+        return location;
+      }
+    }
+  }
 
 
-  return (
-    <Panels id={id} className={className}>
-     
-     <Panel className='locationsIdxleftPanel'>
-       <ul className='locationsIdxUL'>
-         {locations?.map((location, idx) => <LocationIndexItem location={location} key={idx} />)}
-         {children}
-       </ul>
-     </Panel>
-     <Panel className='locationssIdxrightPanel'>
-       {/* map goes here */}
-     </Panel>
-
-   </Panels> 
-  )
+  if (!loaded) {
+    return (
+      <Loading />
+    )
+  } else {
+    return (
+      <Panels id={id} className={className}>
+      
+        <Panel className='locationsIdxleftPanel'>
+          <ul className='locationsIdxUL'>
+            {locations?.map((location, idx) => <LocationIndexItem location={location} key={idx} />)}
+            {children}
+          </ul>
+        </Panel>
+        <Panel className='locationssIdxrightPanel'>
+          <Map />
+        </Panel>
+      </Panels> 
+    )
+  }
 }
 
 export default LocationsIndexPage;
