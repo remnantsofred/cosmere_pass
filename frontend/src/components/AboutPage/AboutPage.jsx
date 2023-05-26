32f0723a9a825cb45264d9190aa02cc50fbc10b7
remnantsofred <remnantsofred@gmail.com>
2023-05-26 11:45:56 -0700

    Update About page paragraph text

--- frontend/src/components/AboutPage/AboutPage.jsx
+++ frontend/src/components/AboutPage/AboutPage.jsx
@@ -9,8 +9,20 @@ export const AboutPage = ({id='', className="about-page"}) => {
   return (
     <Panel id={id} className={`${className} about-page-container`}>
       <Panels className={`${className} about-page-left-panel`}>
-        Hi! My name is Daphne 
-        
+        <h4>About the developer & project</h4>
+        <br />
+        <p className='about-page-left-panel-p'>
+          Hi! My name is Daphne and I am a software engineer with an HR background. I first fell in love with fitness while exploring group 
+          classes through ClassPass, so when it was time to select an application to clone for our full-stack project at App Academy, I knew 
+          what I wanted to clone. Additionally, I knew I wanted to add a twist to it, so I decided to theme my clone after Brandon Sanderson's 
+          Cosmere universe. Thus, Cosmerepass. Brandon Sanderson is my husband's favorite author that he also got me into, so his books and 
+          universe are very meaningful to me. It was a lot of fun to blend the two together.
+        </p>
+        <br />
+        <p className='about-page-left-panel-p'>
+          Cosmerepass was developed using a Ruby on Rails backend, PostgreSQL for the database, and a React/Redux frontend. It also utilizes
+          the Google Maps API.
+        </p>
         <br />
         <br />
         <br />
