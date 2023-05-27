a209017cea40df92edb964f8a963688778b560e3
remnantsofred <remnantsofred@gmail.com>
2023-05-26 17:06:28 -0700

    Set up layout for account page

--- frontend/src/components/AccountPage/AccountPage.jsx
+++ frontend/src/components/AccountPage/AccountPage.jsx
@@ -2,25 +2,39 @@ import './AccountPage.css';
 import Panel from '../panel/Panel';
 import Panels from '../panels';
 import Row from '../row/Row';
+import { useSelector } from 'react-redux';
 
 export const AccountPage = () => {
+  const currentUser = useSelector(state => state.session.user);
 
 
   return(
-    <Panels className='AccountPagePanels'>
-      <Panel className='AcctPagePanelL'>
-        
-        <Row>Name</Row>
-        <Row>Upcoming</Row>
-        <Row>Attended</Row>
-        <Row>Favorites</Row>
-        <Row>Reviews</Row>
-      </Panel>
-      <Panel className='AcctPagePanelR'>
-        
+    <Panel className='acct-page-container'>
+      <Panels className='acct-page-panel-L'>
+        <Row className='acct-page-title-row-welcome-banner'>Welcome back, <h6 className='username-header'>{currentUser.username}</h6></Row>
+        <ul className='acct-page-title-ul'>
+          <li className='acct-page-title-li'>
+            <p>Upcoming reservations</p>
+          </li>
+          <li className='acct-page-title-li'>
+            <p>Past reservations</p>
+          </li>
+          <li className='acct-page-title-li'>
+            <p>Favorites</p>
+          </li>
+          <li className='acct-page-title-li'>
+            <p>Reviews</p>
+          </li>
+        </ul>
+      </Panels>
+      <Panels className='acct-page-panel-R'>
+        <div className='acct-page-main-content-container'>
 
-      </Panel>
-    </Panels>
+
+        </div>
+
+      </Panels>
+    </Panel>
   )
 }
 
