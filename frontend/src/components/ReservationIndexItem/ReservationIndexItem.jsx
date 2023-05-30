ddc17945ea97f4fc2b37da084fd44172b204d1be
remnantsofred <remnantsofred@gmail.com>
2023-05-30 16:54:06 -0700

    Update ReservationIndexItem content

--- frontend/src/components/ReservationIndexItem/ReservationIndexItem.jsx
+++ frontend/src/components/ReservationIndexItem/ReservationIndexItem.jsx
@@ -3,6 +3,7 @@ import Row from '../row/Row';
 import Panel from '../panel/Panel';
 import { useSelector } from 'react-redux';
 import { getLocation } from '../../store/location';
+import { formatDate, formatTime, timeBetween, formatDateWithDay, formatDateWithDayShortAlt } from '../../utils/date_util';
 
 export const ReservationIndexItem = ({reservation, type}) =>{
   // const location = useSelector(getLocation(reservation.locationId));
@@ -15,9 +16,10 @@ export const ReservationIndexItem = ({reservation, type}) =>{
     <Row className='reservation-index-item-container'>
       <img src={reservation.lesson_image_url} alt={reservation.locationName} className='LocShowImg' />
       <Panel className='reservation-index-item-info-panel'>
-        {reservation.lessonTitle}
-        {reservation.locationName}
-        {reservation.startTime} - {reservation.endTime}
+        <p className='lessonDateIdxItmTime startTimeLoc'>{reservation.lessonTitle}</p>
+        <p className='lessonDateIdxItmTime startTime'>{reservation.locationName}</p>
+        <p className="lessonDateIdxItmTime duration">{formatDateWithDayShortAlt(reservation.startTime)}</p>
+        <p className="lessonDateIdxItmTime duration">{formatTime(reservation.startTime)}-{formatTime(reservation.endTime)}</p>
       </Panel>
       {type == 'upcoming' && <Panel className='reservation-index-item-button-panel'>
         <button className='lessonDateIdxItmReserve reservation-index-button'> Bring a friend </button>
