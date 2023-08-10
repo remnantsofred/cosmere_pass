067e32e11fceed215a4c4afe1309ec91a90d29e6
remnantsofred <remnantsofred@gmail.com>
2023-08-09 18:06:05 -0700

    Finish account page mobile styling aside from cancel modal

--- frontend/src/components/ReservationIndexItem/ReservationIndexItem.jsx
+++ frontend/src/components/ReservationIndexItem/ReservationIndexItem.jsx
@@ -29,10 +29,10 @@ export const ReservationIndexItem = ({reservation, type, handleCancel}) =>{
       <Panel className='reservation-index-item-info-panel'>
         <p className='lessonDateIdxItmTime startTimeLoc'>{reservation.lessonTitle}</p>
         <p className='lessonDateIdxItmTime startTime'>{reservation.locationName} - {getWorld(reservation.locationName)}</p>
+        <p className="lessonDateIdxItmTime duration-mobile">{formatDateWithDayShortAlt(reservation.startTime)}  ·  {formatTime(reservation.startTime)} - {formatTime(reservation.endTime)}</p>
         <br />
         <p className="lessonDateIdxItmTime duration">{formatDateWithDayShortAlt(reservation.startTime)}</p>
         <p className="lessonDateIdxItmTime duration">{formatTime(reservation.startTime)} - {formatTime(reservation.endTime)}</p>
-        <p className="lessonDateIdxItmTime duration-mobile">{formatDateWithDayShortAlt(reservation.startTime)}  ·  {formatTime(reservation.startTime)} - {formatTime(reservation.endTime)}</p>
       </Panel>
       {type === 'upcoming' && <Panel className='reservation-index-item-button-panel'>
         <button className='reservation-index-button' onClick={ () => handleCancel(reservation, 'invite')}> Bring a friend </button>
